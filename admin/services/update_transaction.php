040d5dbd9a51d1141d8b72e86024167ad41da4f4
Bkmsx <tien@novum.capital>
2018-07-23 00:06:57 +0700

    Add update user, transaction services

--- admin/services/update_transaction.php
+++ admin/services/update_transaction.php
@@ -1,6 +1,21 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/paths.php');
 require_once(WPAY_PATH.'/services/utils/mysqli_connect.php');
+$transaction_id = $_POST['transaction_id'];
+$user_email = $_POST['user_email'];
+$currency = $_POST['currency'];
+$amount = $_POST['amount'];
+$address = $_POST['address'];
+$token_amount = $_POST['token_amount'];
+$token_bonus = $_POST['token_bonus'];
+$status = $_POST['status'];
+$conversion_rate = $_POST['conversion_rate'];
 
+$update_transaction = "update users set user_email='$user_email', currency='$currency',
+                amount='$amount', address='$address',
+                token_amount='$token_amount', token_bonus='$token_bonus',
+                status='$status', conversion_rate='$conversion_rate',
+                 where transaction_id='$transaction_id'";
+mysqli_query($dbc, $update_transaction);
 mysqli_close($dbc);
 ?>
--- admin/services/update_user.php
+++ admin/services/update_user.php
@@ -1,6 +1,22 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/paths.php');
 require_once(WPAY_PATH.'/services/utils/mysqli_connect.php');
-$update_user = "update users set ";
+$user_id = $_POST['user_id'];
+$email = $_POST['email'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$date_birth = $_POST['date_birth'];
+$citizenship = $_POST['citizenship'];
+$country = $_POST['country'];
+$status = $_POST['status'];
+$wallet_address = $_POST['wallet_address'];
+$token_number = $_POST['token_number'];
+
+$update_user = "update users set email='$email', first_name='$first_name',
+                last_name='$last_name', date_birth='$date_birth',
+                citizenship='$citizenship', country='$country',
+                status='$status', wallet_address='$wallet_address', 
+                token_number='$token_number' where user_id='$user_id'";
+mysqli_query($dbc, $update_user);
 mysqli_close($dbc);
 ?>
